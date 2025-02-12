58a5c7061c527800f56113284159077f24155bfa
Akshat Jain <jainAkshat_02@protonmail.com>
2025-02-13 01:04:43 +0530

    progress

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () =>{
+taskBox= document.getElementById('taskBox');
+rescheduleBtn= document.getElementById('rescheduleBtn');
+icons= document.getElementById('icons');
+
+tasks=[];
+taskBox.addEventListener('click', () =>{
+    let addbtn=document.createElement('button')
+    // alert('taskBox clicked');
+    // taskBox.innerHTML= '';
+    // let task= document.createElement('input');
+    // let taskText= task.value.trim();
+    // if (taskText==='') {
+    //     return
+    // }else{
+    //     tasks.push(taskText);
+    // }
+    // console.log(tasks);
+    
+    
+})
+    
+    
+});
